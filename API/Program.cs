772dbcccd6632adf0021adc42ed33e06b3730497
Tran Viet Hoang <hoangdl939@gmail.com>
2024-01-20 02:07:59 +0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -24,7 +24,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//setting up cors
+app.UseCors(opt => 
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
